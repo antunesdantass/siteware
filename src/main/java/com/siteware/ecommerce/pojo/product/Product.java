b2547ecbc5cd578911c98081cd1a9ee6d2a82b4b
Antunes Dantas <antunesdantas@outlook.com>
2019-07-09 15:14:03 -0300

    JPA annotations at Product

--- src/main/java/com/siteware/ecommerce/pojo/product/Product.java
+++ src/main/java/com/siteware/ecommerce/pojo/product/Product.java
@@ -1,8 +1,14 @@
 package com.siteware.ecommerce.pojo.product;
 
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  * Product entity, responsible for modeling how a product is in the system.
@@ -15,10 +21,17 @@ public class Product implements Serializable {
 
     private static final long serialVersionUID = -265665862922221302L;
 
+    @Id
+    @Column(name = "ID_PRODUCT")
     private Long id;
 
+    @Column(name = "NO_NAME")
+    @Length(max = 500)
+    @NotNull
     private String name;
 
+    @Column(name = "NR_PRICING")
+    @NotNull
     private Double pricing;
 
     public Product() {}
@@ -50,5 +63,18 @@ public class Product implements Serializable {
 
     public void setPricing(Double pricing) {
         this.pricing = pricing;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
