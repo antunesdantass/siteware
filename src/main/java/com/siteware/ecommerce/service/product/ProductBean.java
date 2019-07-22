60fe560ccc6a32b48d4ddb88df15435855c57b67
Antunes Dantas <antunesdantas@outlook.com>
2019-07-22 15:51:34 -0300

    fixing getProductsByids whenn the list is empty

--- src/main/java/com/siteware/ecommerce/service/product/ProductBean.java
+++ src/main/java/com/siteware/ecommerce/service/product/ProductBean.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -54,6 +55,6 @@ public class ProductBean implements ProductService {
 
     @Override
     public List<Product> getProductsById(List<Long> ids) {
-        return productDAO.findProductsByListOfId(ids);
+        return ids.isEmpty()? new ArrayList<>() : productDAO.findProductsByListOfId(ids);
     }
 }
